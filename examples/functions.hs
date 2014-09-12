e2ded0eadb58739316653ece17863296d0284f38
Todd Mohney <toddmohney@gmail.com>
2014-09-12 11:44:58 -0400

    Sum even fibonacci numbers (Problem 2)

--- examples/functions.hs
+++ examples/functions.hs
@@ -7,8 +7,3 @@ removeNonUppercase str = [c | c <- str, c `elem` ['A'..'Z']]
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
 
-
-fibonacci :: Int -> Int
-fibonacci 0 = 0
-fibonacci 1 = 1
-fibonacci x = fibonacci(x-1) + fibonacci(x-2)
---
+++ problem_2.hs
@@ -0,0 +1,11 @@
+{- By considering the terms in the Fibonacci sequence whose values  -}
+{- do not exceed four million, find the sum of the even-valued terms. -}
+
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib x = fib(x-1) + fib(x-2)
+
+sumOfEvenFibonacciNumbers = sum [x | x <- takeWhile (<= 1000000) fibs, even x]
+  where
+    fibs = [fib x | x <- [1..]]
