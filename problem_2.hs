c51f2a9b33cc7410b59032e528431e3e0c7a0063
Todd Mohney <toddmohney@gmail.com>
2014-09-12 11:59:07 -0400

    Add missing function type

--- problem_2.hs
+++ problem_2.hs
@@ -6,6 +6,7 @@ fib 0 = 0
 fib 1 = 1
 fib x = fib(x-1) + fib(x-2)
 
+sumOfEvenFibonacciNumbers :: Int
 sumOfEvenFibonacciNumbers = sum [x | x <- takeWhile (<= 1000000) fibs, even x]
   where
     fibs = [fib x | x <- [1..]]
